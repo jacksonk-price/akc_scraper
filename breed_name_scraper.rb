2b63948137b7686ee4d3f6582a3720f7460b56ca
Jackson Price <price.jacksonk@gmail.com>
2023-01-03 19:33:16 -0600

    Refactor load more loop

--- breed_name_scraper.rb
+++ breed_name_scraper.rb
@@ -1,29 +1,46 @@
 #!/usr/bin/env ruby
 require "selenium-webdriver"
 
-letters = ('A'..'Y').to_a
-driver = Selenium::WebDriver.for :firefox
-names = []
+$DRIVER = Selenium::WebDriver.for :firefox
 
-letters.each do |letter|
-  driver.navigate.to("https://www.akc.org/dog-breeds/?letter=#{letter}")
-  puts '-' * 50
-  puts "navigating to https://www.akc.org/dog-breeds/?letter=#{letter}"
-  puts '-' * 50
-  wait = Selenium::WebDriver::Wait.new(:timeout => 20)
-  
-  while true do
-    begin
-      driver.find_element(:id, 'load-more-btn').click
+def scrape_it
+  names = []
+  letters = ('A'..'Y').to_a
+  letters.each do |letter|
+    $DRIVER.navigate.to("https://www.akc.org/dog-breeds/?letter=#{letter}")
+    puts '-' * 50
+    puts "navigating to https://www.akc.org/dog-breeds/?letter=#{letter}"
+    puts '-' * 50
+    wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+    wait.until {
+      $DRIVER.find_element(:class, 'resources-billboard__text')
+    }
+    while load_more_btn_exists? do
+      $DRIVER.find_element(:id, 'load-more-btn').click
       puts 'load more clicked...'
-    rescue => e
-      sleep 3
-      puts "starting name grab for category #{letter}..."
-      driver.find_elements(:class, 'breed-type-card__title').each do |title|
-        puts "grabbing name #{title.text}"
-        names << title.text
-      end
-      break
+    end
+
+    sleep 3
+    puts "starting name grab for category #{letter}..."
+    $DRIVER.find_elements(:class, 'breed-type-card__title').each do |title|
+      puts "grabbing name #{title.text}"
+      names << title.text
     end
   end
+
+
+  puts names.inspect
+  puts '-' * 50
+  puts "#{names.count} total names collected"
+  puts '-' * 50
+  $DRIVER.close
 end
+def load_more_btn_exists?
+  begin
+    true if $DRIVER.find_element(:id, 'load-more-btn')
+  rescue => e
+    false
+  end
+end
+
+scrape_it

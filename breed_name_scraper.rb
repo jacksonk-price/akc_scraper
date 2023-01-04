9d192c444692942d56470530788bf8c9cff576e3
Jackson Price <price.jacksonk@gmail.com>
2023-01-03 19:10:29 -0600

    Add main functionality

--- breed_name_scraper.rb
+++ breed_name_scraper.rb
@@ -11,4 +11,19 @@ letters.each do |letter|
   puts "navigating to https://www.akc.org/dog-breeds/?letter=#{letter}"
   puts '-' * 50
   wait = Selenium::WebDriver::Wait.new(:timeout => 20)
+  
+  while true do
+    begin
+      driver.find_element(:id, 'load-more-btn').click
+      puts 'load more clicked...'
+    rescue => e
+      sleep 3
+      puts "starting name grab for category #{letter}..."
+      driver.find_elements(:class, 'breed-type-card__title').each do |title|
+        puts "grabbing name #{title.text}"
+        names << title.text
+      end
+      break
+    end
+  end
 end

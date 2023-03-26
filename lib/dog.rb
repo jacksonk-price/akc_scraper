babcd720b40b0a8686350b521ed4ba2947fedc51
Jackson Price <price.jacksonk@gmail.com>
2023-03-26 17:14:56 -0500

    Remove: unneeded comments and logs

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,6 @@ class Dog
       end
     when 2
       female_text = weight_elems[1]
-      puts 'HERE GIRL'
       puts female_text.match(pattern).inspect
       if female_text.match(pattern)
         if $2.nil?
@@ -63,7 +62,6 @@ class Dog
       end
 
       male_text = weight_elems[0]
-      puts 'HERE BOY'
       puts male_text.match(pattern).inspect
       if male_text.match(pattern)
         if $2.nil?
@@ -75,60 +73,6 @@ class Dog
       end
 
       weight_hash
-
-    # when 1
-    #   if weight_elems[0].include? 'pounds' # example => ["45- 70 pounds"]
-    #     weight_text = remove_whitespace(weight_elems[0]).gsub('pounds', '') # remove whitespace and pounds => ["45-70"]
-    #     split_weight_text = weight_text.split('-') # split at hyphen => ["45", "70"]
-    #     weight_hash[:min_weight] = split_weight_text[0] # store in hash for min => "45"
-    #     weight_hash[:max_weight] = split_weight_text[1] # store hash for max => "70"
-    #   elsif weight_elems[0].include? 'lbs' # example => ["45- 70 lbs"]
-    #     weight_text = remove_whitespace(weight_elems[0]).gsub('lbs', '') # remove whitespace and lbs => ["45-70"]
-    #     split_weight_text = weight_text.split('-') # split at hyphen => ["45", "70"]
-    #     weight_hash[:min_weight] = split_weight_text[0] # store in hash for min => "45"
-    #     weight_hash[:max_weight] = split_weight_text[1] # store in hash for max => "70"
-    #   end
-    # when 2
-    #   if weight_elems[0].include? 'pounds' # example => ["45- 70 pounds (male)", "25- 55 pounds (female)"]
-    #     if weight_elems[0].include? 'under'
-    #       weight_text = remove_whitespace(weight_elems[1])
-    #       split_weight_text = weight_text.split('-')
-    #       weight_hash[:min_weight] = split_weight_text[0]
-    #       weight_hash[:max_weight] = split_weight_text[1].split('p')[0]
-    #     else
-    #       male_weight_text = remove_whitespace(weight_elems[0]) # remove whitespace for male value => ["45-70pounds(male)"]
-    #       if male_weight_text.include? '-'
-    #         split_male_weight_text = male_weight_text.split('-') # split values => ["70", "130pounds(male)"]
-    #         max_male_weight = split_male_weight_text[1].split('p')[0] # split max val at the p and select first elem=> ["130"]
-    #       else
-    #         max_male_weight = male_weight_text.split('p')[0]
-    #       end
-    #       female_weight_text = remove_whitespace(weight_elems[1])
-    #       if female_weight_text.include? '-'
-    #         min_female_weight = female_weight_text.split('-')[0]
-    #       else
-    #         min_female_weight = female_weight_text.split('p')[0]
-    #       end
-    #       weight_hash[:min_weight] = min_female_weight
-    #       weight_hash[:max_weight] = max_male_weight
-    #     end
-    #   elsif weight_elems[0].include? 'lbs'
-    #     male_weight_text = remove_whitespace(weight_elems[0])
-    #     if male_weight_text.include? '-'
-    #       split_male_weight_text = male_weight_text.split('-') # => ["70", "130pounds"]
-    #       max_male_weight = split_male_weight_text[1].to_s.split('l')[0]
-    #     else
-    #       max_male_weight = male_weight_text.split('l')[0]
-    #     end
-    #     female_weight_text = remove_whitespace(weight_elems[1])
-    #     if female_weight_text.include? '-'
-    #       min_female_weight = female_weight_text.split('-')[0]
-    #     else
-    #       min_female_weight = female_weight_text.split('l')[0]
-    #     end
-    #     weight_hash[:min_weight] = min_female_weight
-    #     weight_hash[:max_weight] = max_male_weight
-    #   end
     when 3
       weight_text = remove_whitespace(weight_elems[2])
       weight_hash[:min_weight] = weight_text.split('-')[0]
